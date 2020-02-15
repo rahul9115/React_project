4663215c4de99d1981f101a00aa0f0d1c6eac563
rahul9115 <46648019+rahul9115@users.noreply.github.com>
2020-02-15 14:12:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+const PORT = process.env.PORT || 5000;
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
     res.send("Hello");
 });
-const PORT = process.env.PORT || 5000;
 app.listen(5000)
