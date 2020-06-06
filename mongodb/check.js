ce04c5509b7f62f9eda5b183f620250d98973b15
rahul <rahul9115>
2020-06-06 12:02:50 +0530

    Modififed

--- mongodb/check.js
+++ mongodb/check.js
@@ -1,25 +1,36 @@
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://rahul9115:nanaamma@cluster0-d6yk7.mongodb.net/test?retryWrites=true&w=majority";
+const uri = "mongodb+srv://rahul:rahul@cluster0.rpfjy.mongodb.net/<dbname>?retryWrites=true&w=majority";
 module.exports =(myobj)=>{ 
   MongoClient.connect(uri, (err, db)=> {
   if (err) throw err;
   var dbo = db.db("mongo");
+  $exists=false;
   var collection=dbo.collection("customers");
-  collection.find((myobj,{$exists: true}), function(err, doc) //find if a value exists
-                {     
-                    if(doc) //if it does
-                    {
-                        console.log("Already present"); // print out what it sends back
-                    }
-                    else if(!doc) // if it does not 
-                    {
-                        dbo.collection("customers").insertOne(myobj, function(err, res) {
-                        if (err) throw err;
-                        console.log("1 document inserted");
-                        db.close();
-                      });
-                
-                    }
-                });
+  
+  var value=collection.find(myobj);
+value.count(function (err, num) {
+    if(err) {
+        return console.log(err);
+    }
+    console.log(num)
+    
+
+  
+            if(num>0) //if it does
+            {
+                console.log("Already present"); // print out what it sends back
+            }
+            if(num===0) // if it does not 
+            {
+                dbo.collection("customers").insertOne(myobj, function(err, res) {
+                if (err) throw err;
+                console.log("1 document inserted");
+                db.close();
+              });
+        
+            }
+              
+ 
+    });
   });
 }
